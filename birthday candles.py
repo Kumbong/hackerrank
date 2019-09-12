fae5bbace37666a85c2ab6c62501837e15a04507
kumbonghermann@gmail.com <kumbonghermann@gmail.com>
2019-09-12 08:36:27 +0000

    birthday candles

---
+++ birthday candles.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    max = -1
+    count =0
+
+    for num in ar:
+        if num>max:
+            max= num
+            count =1
+        elif num==max:
+            count+=1
+        
+    return count
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
