aa53592894b3071b5f81d52c3b9d2f5255df032c
kumbonghermann@gmail.com <kumbonghermann@gmail.com>
2019-09-12 08:25:43 +0000

    simple array sum

---
+++ simple array sum.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    sum =0
+    for num in ar:
+        sum+=num
+
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
