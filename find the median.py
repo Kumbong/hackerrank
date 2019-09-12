f3987b60ce99b7657af6447ae7e70f05bff45de0
kumbonghermann@gmail.com <kumbonghermann@gmail.com>
2019-09-12 09:05:45 +0000

    find the median

---
+++ find the median.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the findMedian function below.
+def findMedian(arr):
+    arr.sort()
+    return arr[len(arr)//2]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
