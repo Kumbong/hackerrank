23618081fd0698e0e5ff670cc8b696a662f6469f
kumbonghermann@gmail.com <kumbonghermann@gmail.com>
2019-09-12 08:54:50 +0000

    angry professor

---
+++ angry professor.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the angryProfessor function below.
+def angryProfessor(k, a):
+    count = 0
+
+    for t in a:
+        if t<=0:
+            count+=1
+
+    if count >= k:
+        return 'NO'
+    
+    else:
+        return 'YES'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
