544c0396144ec9fbe73721f8f2e8fe9c71e6546b
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-18 05:48:24 +0000

    weighted uniform strings

---
+++ weighted uniform strings.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import string
+
+# Complete the pangrams function below.
+def pangrams(s):
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    s = s.lower()
+    for letter in letters:
+        if letter not in s:
+            return "not pangram"
+
+    return "pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
