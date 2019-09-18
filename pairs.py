6c58a8f03cb1adb396aaa218f00830e4430853ca
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-18 13:02:30 +0000

    pairs

---
+++ pairs.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the pairs function below.
+def pairs(k, arr):
+    return len(set(arr) & set(x + k for x in arr))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
