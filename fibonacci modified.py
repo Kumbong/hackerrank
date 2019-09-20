6fb925576e0cce7562dd4b7f03877d464aae99c3
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-20 02:13:25 +0000

    fibonnaci modified

---
+++ fibonacci modified.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    for i in range(3,n+1):
+        t_n = t1 +(t2)**2
+        temp = t2
+        t2 = t_n
+        t1 = temp
+
+    return t_n
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
