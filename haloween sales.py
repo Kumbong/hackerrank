66239a661cdb790db2d690f6967c3f9ff91a352f
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-21 09:11:17 +0000

    haloween sales

---
+++ haloween sales.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    count =0
+    i = 0
+    amt=p
+    while s>=p:
+        p=amt-i*d
+        if p>m:
+            i+=1
+        else:
+            p=m
+        
+        s-=p
+        count+=1
+    
+    return count
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
