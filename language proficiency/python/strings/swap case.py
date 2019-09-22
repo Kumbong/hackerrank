4272fd981af0417032140cea4b7a627db27c2866
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-22 23:34:56 +0000

    swap case

---
+++ language proficiency/python/strings/swap case.py
@@ -0,0 +1,7 @@
+def swap_case(s):
+    return ''.join([c.lower() if c.isupper() else c.upper() for c in s])
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
