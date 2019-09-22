6b2bcd7f9f5ec416b6d6f92e0d92a46af55f5b84
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-22 22:33:43 +0000

    write a function, print function

---
+++ language proficiency/python/Introduction/print function.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    print(*range(1, n+1), sep='')
+
+
---
+++ language proficiency/python/Introduction/write a function.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    leap = False
+    
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                leap= True
+            else:
+                leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+    
+    return leap
+
+year = int(input())
