dabda0269b3ad42589446f404de1435b518d3247
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-22 13:03:40 +0000

    jim and the orders

---
+++ jim and the orders.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jimOrders function below.
+def jimOrders(orders):
+    
+    for i in range(len(orders)):
+        orders[i].append(i+1)
+    
+    orders = sorted(orders,key=lambda x: (x[0]+x[1],x[2]))
+
+    res = []
+
+    for el in orders:
+        res.append(el[2])
+
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    orders = []
+
+    for _ in range(n):
+        orders.append(list(map(int, input().rstrip().split())))
+
+    result = jimOrders(orders)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
