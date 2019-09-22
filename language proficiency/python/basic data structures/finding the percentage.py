e3e22b9867e866231d1a959d338bb2cf522852a0
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-22 23:23:12 +0000

    finding the percentage

---
+++ language proficiency/python/basic data structures/finding the percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    marks = student_marks[query_name]
+    print("{:.2f}".format(round(sum(marks)/len(marks),2)))
+
+
