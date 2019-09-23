7f3b5bd47844ca3cdc5240dab0d6d81495cc4987
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-23 02:23:18 +0000

    capitalize

---
+++ language proficiency/python/strings/capitalize.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    return ' '.join(i.capitalize() for i in s.split(' '))
+if __name__ == '__main__':
