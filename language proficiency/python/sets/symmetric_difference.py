6aeb38cf729539083965e84906e971c82bd09e05
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-23 02:03:06 +0000

    symmetric_difference

---
+++ language proficiency/python/sets/symmetric_difference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+_ = input()
+
+a = set(input().split())
+_=input()
+b = set(input().split())
+
+print(len(a.union(b))-len(a.intersection(b)))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
