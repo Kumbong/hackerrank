97c48c74ce09720c5e9ef65f04102f48aea4d89e
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-25 03:47:08 +0000

    repeated string

---
+++ problem solving/algorithms/repeated string.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def repeatedString(s, n):
+ 
+    return s.count('a')*(n//len(s)) + s[:n%len(s)].count('a')
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
