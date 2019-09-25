ef2627fe1fb2ab180ee8d0734c2a089444f5748c
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-25 05:02:23 +0000

    flatland space stations

---
+++ problem solving/algorithms/flatland space stations.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the flatlandSpaceStations function below.
+def flatlandSpaceStations(n, c):
+    c.sort()
+    m=-1
+
+    for i in range(1,len(c)):
+        if c[i]-c[i-1]>m:
+            m=c[i]-c[i-1]
+
+    return max(m//2,n-c[-1]-1,c[0])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = flatlandSpaceStations(n, c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
