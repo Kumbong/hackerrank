d0975c39346930357bdb446d7f178ea7bc2ee1bb
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-25 03:37:01 +0000

    marc's cakewalk

---
+++ problem solving/algorithms/marc's cakewalk.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marcsCakewalk function below.
+def marcsCakewalk(calorie):
+    calorie.sort(reverse=True)
+
+    min_calories = 0
+    for i in range(len(calorie)):
+        min_calories+=(2**i)*calorie[i]
+
+    return min_calories
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
