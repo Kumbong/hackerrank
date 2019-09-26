4fa571f79d0954e2b498b21076294d54eecd2269
Kumbong Hermann <kumbonghermann@gmail.com>
2019-09-26 02:17:49 +0000

    the love letter mystery

---
+++ problem solving/algorithms/the love letter mystery.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the theLoveLetterMystery function below.
+def theLoveLetterMystery(s):
+    return sum([abs(ord(s[i])-ord(s[len(s)-1-i])) for i in range(len(s)//2)])
+  
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
