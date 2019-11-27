1b706a21a2e093299bb0324fe94c27a9fb6b2a03
Bismarck Bamfo <Bismarckbamfo91@gmail.com>
2019-11-27 20:42:01 +0000

    Add files via upload

--- Java/Solution.java
+++ Java/Solution.java
@@ -1,9 +1,20 @@
+import java.util.*;
+
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named 
-        Solution. */
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
     }
 }
+
